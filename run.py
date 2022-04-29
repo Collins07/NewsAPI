520b09275bf3ea62a405317177b367077a63b690
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-04-30 01:29:03 +0300

    added API key authentication

--- run.py
+++ run.py
@@ -8,6 +8,10 @@ from newsapi import NewsApiClient
 @app.route('/')
 @app.route('/home')
 def home():
+
+    newsapi = NewsApiClient(api_key="1111f8f3bf334cc6a0af77fa30f68e29")
+
+
     return render_template('index.html')
 
 
