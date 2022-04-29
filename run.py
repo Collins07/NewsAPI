6c6f7b7b594f9497d1e23f1f91e9d249ef863336
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-04-30 02:06:58 +0300

    bbc news as source

--- run.py
+++ run.py
@@ -11,6 +11,8 @@ def home():
 
     newsapi = NewsApiClient(api_key="1111f8f3bf334cc6a0af77fa30f68e29")
 
+    top_headlines = newsapi.get_top_headlines(sources= "bbc-news" )
+
 
     return render_template('index.html')
 
