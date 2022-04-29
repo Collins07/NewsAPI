6c05db51ef923cc4ff1800d518a402338a1961a8
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-04-29 21:47:45 +0300

    render template

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return ('home.html')
+    return render_template('home.html')
