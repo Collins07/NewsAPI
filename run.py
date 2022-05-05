1e3e9600fd4c77b959920fc1f1308f4ef9f7e022
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-05-05 14:59:49 +0300

    requirements file

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
-app = Flask(__name__)
-
 
 from newsapi import NewsApiClient
 
 
+app = Flask(__name__)
+
+
 @app.route('/')
-@app.route('/home')
 def home():
 
     newsapi = NewsApiClient(api_key="1111f8f3bf334cc6a0af77fa30f68e29")
